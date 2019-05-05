ff2611e53fbbef2da3d9c336fa8566807ccae0c7
Anastasia Hahanova <anastasia_hahanova@epam.com>
2019-05-05 22:39:37 +0300

    vue setup

---
+++ i1/src/components/home/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="home-menu">
+      <ul class="home-menu__list">
+        <li class="home-menu__option"><a href="./mainCourses.html" class="home-menu__link" title="Первые блюда">Первые блюда</a></li>
+        <li class="home-menu__option"><a href="#" class="home-menu__link" title="Вторые блюда">Вторые блюда</a></li>
+        <li class="home-menu__option"><a href="#" class="home-menu__link" title="Пироги">Пироги</a></li>
+        <li class="home-menu__option"><a href="#" class="home-menu__link" title="Десерты">Десерты</a></li>
+        <li class="home-menu__option"><a href="#" class="home-menu__link" title="Соусы">Соусы</a></li>
+      </ul>
+    </section>
+</template>
+
+<script>
+export default {
+  name: 'H',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+@import './home';
+</style>
